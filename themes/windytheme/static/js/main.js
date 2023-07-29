392417fa890725f5184a8094b48b9849ba5eb5af
Zach <hello@zacx.dev>
2023-07-29 13:14:43 -0500

    add spinner and thank you message

--- themes/windytheme/static/js/main.js
+++ themes/windytheme/static/js/main.js
@@ -1,29 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const openModal = document.getElementById('openModal');
-  const closeModal = document.getElementById('closeModal');
   const modal = document.getElementById('modal');
   const overlay = document.getElementById('overlay');
   const submitEmail = document.getElementById('submitEmail');
   const emailInput = document.getElementById('emailInput');
   const formEl = document.getElementById('subscribeForm');
-  const thankyouModal = document.getElementById('thankyouModal');
+  const thankyouMessage = document.getElementById('thankyouMessage');
 
   function hideModal() {
-    // modal.classList.add('hidden');
     modal.classList.add('slide-up');
     modal.classList.remove('slide-down');
   }
 
   document.body.addEventListener('click', function(e) {
-    if (e.target.id === 'openModal') {
+    const clickedId = e.target.id
+
+    if (clickedId === 'openModal') {
       modal.classList.remove('hidden');
       modal.classList.remove('slide-up');
       modal.classList.add('slide-down');
+
+      return
+    }
+
+    if (clickedId === 'closeModal') {
+      hideModal()
+
+      return
     }
   });
 
   overlay.addEventListener('click', hideModal);
-  closeModal.addEventListener('click', hideModal);
 
   window.addEventListener('keydown', function(event) {
     if (event.key === 'Escape') {
@@ -37,6 +43,9 @@ document.addEventListener("DOMContentLoaded", function() {
       return
     }
 
+    document.getElementById('spinner-wrap').classList.remove('hidden')
+    document.getElementById('submit-text-wrap').classList.add('hidden')
+
     const data = new FormData(formEl)
 
     fetch('https://7737a90f.sibforms.com/serve/MUIFALeTVOJo5SmMvrMKFp5w8oKB81Ol0VxQc3sBwKZzfeydpqXVjbOx1MTWuoeCFc_KIdVetnaN7OgSFtb6KpA46vBn6iJezjI4VD_Kt8MMLeVcgC7pO3MQqU_MnjrrrdzuFg-OQXkEC33VYAU9yDwEDC5qcD6Xsxs0COvkpfA7kUq19QkIQ2vKjfglxN8_x0i8wDq_6GDa3gfN?isAjax=1', {
@@ -45,8 +54,9 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     .then(response => response.json())
     .then(data => {
-      hideModal();
-      thankyouModal.classList.remove('hidden');
+      //hideModal();
+      document.getElementById('subscribeForm').classList.add('hidden')
+      thankyouMessage.classList.remove('hidden');
     })
     .catch((error) => {
       console.error('Error:', error);
